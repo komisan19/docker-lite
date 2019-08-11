ce920bd935ea7409cbbb1fa6e493e9c3c35afc15
komisan19 <komiyama6219@gmail.com>
2019-08-12 08:04:27 +0900

    [fix]fix indent

--- cmd/cli.go
+++ cmd/cli.go
@@ -2,25 +2,25 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/urfave/cli"
 	"os"
 	"os/exec"
-	"github.com/urfave/cli"
 )
 
-func Version(c *cli.Context){
+func Version(c *cli.Context) {
 	out, err := exec.Command("docker", "version").Output()
-  if err != nil {
-    fmt.Println(err.Error())
-    os.Exit(1)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
 	}
 	fmt.Println(string(out))
 }
 
-func Ps(c *cli.Context){
+func Ps(c *cli.Context) {
 	out, err := exec.Command("docker", "ps").Output()
-  if err != nil {
-    fmt.Println(err.Error())
-    os.Exit(1)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
 	}
 	fmt.Println(string(out))
 }
--- main.go
+++ main.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-        "github.com/urfave/cli"
-				"os"
-				"go-cli/cmd"
+	"github.com/urfave/cli"
+	"go-cli/cmd"
+	"os"
 )
 
 func main() {
-        app := cli.NewApp()
-        app.Name = "docker-like"
-        app.Usage = "lite docker-cli"
-				app.Version = "0.0.1"
+	app := cli.NewApp()
+	app.Name = "docker-like"
+	app.Usage = "lite docker-cli"
+	app.Version = "0.0.1"
 
-				app.Commands = []cli.Command{
-					{
-									Name:   "version",
-									Action: cmd.Version,
-									Usage:   "docker version",
-					},
-					{
-									Name:   "ps",
-									Action: cmd.Ps,
-									Usage:   "docker ps",
-					},
-				}
-				app.Run(os.Args)
+	app.Commands = []cli.Command{
+		{
+			Name:   "version",
+			Action: cmd.Version,
+			Usage:  "docker version",
+		},
+		{
+			Name:   "ps",
+			Action: cmd.Ps,
+			Usage:  "docker ps",
+		},
+	}
+	app.Run(os.Args)
 }
